d1051213468f7b6dac61053c7c78221a71576e00
nagarajalva5790 <nagarajalva5790@gmail.com>
2020-12-21 13:26:55 +0530

    Updates

---
+++ precache-manifest.bad3afdfe1d617828da183a978d68e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c8a47d2285c3219843d19daa27b3fd",
+    "url": "/shape_calcy/index.html"
+  },
+  {
+    "revision": "d8199410057afa7fd54a",
+    "url": "/shape_calcy/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "d8199410057afa7fd54a",
+    "url": "/shape_calcy/static/js/2.56ba2d8c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/shape_calcy/static/js/2.56ba2d8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a0530750678c0d2b8f",
+    "url": "/shape_calcy/static/js/main.d2d0f150.chunk.js"
+  },
+  {
+    "revision": "5aedffae80a8703923c7",
+    "url": "/shape_calcy/static/js/runtime-main.45b939a6.js"
+  }
+]);
